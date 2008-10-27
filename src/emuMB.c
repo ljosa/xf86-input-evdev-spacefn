ljosa/xf86-input-evdev-spacefn

commit b0737bdbd1f6e601eb4984b6f4cb49279190984c

Clean up program flow - don't call PreInit for "modules" on DEVICE_INIT.
Call the PreInit functions for MB Emulation, wheel emu, and draglock during
PreInit, not on DEVICE_INIT. This way, we only parse the options once and
don't overwrite with defaults when coming back from a VT switch.

src/emuMB.c
@@ -320,15 +320,6 @@ EvdevMBEmuPreInit(InputInfoPtr pInfo)
     RegisterBlockAndWakeupHandlers (EvdevMBEmuBlockHandler,
                                     EvdevMBEmuWakeupHandler,
                                     (pointer)pInfo);
-
-#ifdef HAVE_PROPERTIES
-    XIChangeDeviceProperty(pInfo->dev, prop_mbemu, XA_INTEGER, 8,
-                           PropModeReplace, 1, &pEvdev->emulateMB.enabled,
-                           TRUE);
-    XIChangeDeviceProperty(pInfo->dev, prop_mbtimeout, XA_INTEGER, 16,
-                           PropModeReplace, 1, &pEvdev->emulateMB.timeout,
-                           TRUE);
-#endif
 }
 
 void

src/evdev.c
@@ -1000,13 +1000,6 @@ EvdevOn(DeviceIntPtr device)
     } else
     {
         xf86AddEnabledDevice(pInfo);
-        if ((pEvdev->flags & EVDEV_BUTTON_EVENTS) &&
-            !(pEvdev->flags & EVDEV_INITIALIZED))
-        {
-            EvdevMBEmuPreInit(pInfo);
-            EvdevWheelEmuPreInit(pInfo);
-            EvdevDragLockInit(pInfo);
-        }
         pEvdev->flags |= EVDEV_INITIALIZED;
         device->public.on = TRUE;
     }
@@ -1366,6 +1359,10 @@ EvdevPreInit(InputDriverPtr drv, IDevPtr dev, int flags)
     }
 
     EvdevCacheCompare(pInfo, FALSE); /* cache device data */
+
+    EvdevMBEmuPreInit(pInfo);
+    EvdevWheelEmuPreInit(pInfo);
+    EvdevDragLockInit(pInfo);
 
     return pInfo;
 }

src/emuWheel.c
@@ -308,25 +308,6 @@ EvdevWheelEmuPreInit(InputInfoPtr pInfo)
             "EmulateWheelInertia: %d, "
             "EmulateWheelTimeout: %d\n",
             pInfo->name, pEvdev->emulateWheel.button, inertia, timeout);
-
-#ifdef HAVE_PROPERTIES
-    XIChangeDeviceProperty(pInfo->dev, prop_wheel_emu, XA_INTEGER, 8,
-            PropModeReplace, 1, &pEvdev->emulateWheel.enabled, TRUE);
-    XIChangeDeviceProperty(pInfo->dev, prop_wheel_button, XA_INTEGER, 8,
-            PropModeReplace, 1, &pEvdev->emulateWheel.button, TRUE);
-    XIChangeDeviceProperty(pInfo->dev, prop_wheel_inertia, XA_INTEGER, 8,
-            PropModeReplace, 1, &pEvdev->emulateWheel.inertia, TRUE);
-    XIChangeDeviceProperty(pInfo->dev, prop_wheel_timeout, XA_INTEGER, 16,
-            PropModeReplace, 1, &pEvdev->emulateWheel.timeout, TRUE);
-
-    val[0] = pEvdev->emulateWheel.X.up_button;
-    val[1] = pEvdev->emulateWheel.X.down_button;
-    val[2] = pEvdev->emulateWheel.Y.up_button;
-    val[3] = pEvdev->emulateWheel.Y.down_button;
-    XIChangeDeviceProperty(pInfo->dev, prop_wheel_axismap, XA_INTEGER, 8,
-                           PropModeReplace, 4, val, TRUE);
-
-#endif
 }
 
 #ifdef HAVE_PROPERTIES

commit af096e8c5d8b425f725a37bf4a98e205e70716e9

emuMB: add EvdevMBEmuOn and call from EvdevOn to register wakeup handlers.
This got broken in b0737bdbd1f6e601eb4984b6f4cb49279190984c, when the
EmuMBPreInit call was removed from EvdevOn. As a result, VT switching away and
back wouldn't restore the wakeup handlers and mouse button presses
disappeared.

src/emuMB.c
@@ -317,6 +317,11 @@ EvdevMBEmuPreInit(InputInfoPtr pInfo)
 
     pEvdev->emulateMB.timeout = xf86SetIntOption(pInfo->options,
                                                  "Emulate3Timeout", 50);
+}
+
+void
+EvdevMBEmuOn(InputInfoPtr pInfo)
+{
     RegisterBlockAndWakeupHandlers (EvdevMBEmuBlockHandler,
                                     EvdevMBEmuWakeupHandler,
                                     (pointer)pInfo);

src/evdev.c
@@ -1008,6 +1008,7 @@ EvdevOn(DeviceIntPtr device)
     } else
     {
         xf86AddEnabledDevice(pInfo);
+        EvdevMBEmuOn(pInfo);
         pEvdev->flags |= EVDEV_INITIALIZED;
         device->public.on = TRUE;
     }

src/evdev.h
@@ -127,6 +127,7 @@ BOOL EvdevMBEmuFilterEvent(InputInfoPtr, int, BOOL);
 void EvdevMBEmuWakeupHandler(pointer, int, pointer);
 void EvdevMBEmuBlockHandler(pointer, struct timeval**, pointer);
 void EvdevMBEmuPreInit(InputInfoPtr);
+void EvdevMBEmuOn(InputInfoPtr);
 void EvdevMBEmuFinalize(InputInfoPtr);
 void EvdevMBEmuEnable(InputInfoPtr, BOOL);
 