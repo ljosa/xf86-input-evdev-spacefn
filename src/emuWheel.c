0f16065b00436c5df48af6e1d6a18e2ed27a12fd
Peter Hutterer <peter.hutterer@who-t.net>
2013-08-28 14:23:49 +1000

    Remove a comment
    This comment is now in the wrong place. It was moved when abs support for
    wheel emulation was added but is now only confusing. Remove it altogether,
    the code is quite obvious what it does.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- src/emuWheel.c
+++ src/emuWheel.c
@@ -115,7 +115,6 @@ EvdevWheelEmuFilterMotion(InputInfoPtr pInfo, struct input_event *pEv)
                 return TRUE;
         }
 
-	/* We don't want to intercept real mouse wheel events */
 	if(pEv->type == EV_ABS) {
 	    int axis = pEvdev->abs_axis_map[pEv->code];
 	    oldValue = valuator_mask_get(pEvdev->vals, axis);
